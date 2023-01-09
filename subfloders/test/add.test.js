102643d5f113859a9ad920f08b60124131f56890
Riyaz Pasha <38011438+riyaz-pasha@users.noreply.github.com>
2023-01-09 16:09:15 +0530

    [Riyaz] Add. 1 (#1)
    * [Riyaz] Add. 1
    
    * [Riyaz] Add. new test

---
+++ subfloders/test/add.test.js
@@ -0,0 +1,10 @@
+const { assert } = require('chai')
+
+describe('Array', function () {
+    describe('#indexOf()', function () {
+      it('should return -1 when the value is not present', function () {
+        var teaServed = true;
+        assert.isTrue(teaServed, 'the tea has been served');
+      });
+    });
+  });
